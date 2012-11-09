53ec10d3932c15a84dfc69716b81bd6a48b165e3
mokeefemomoney <mokeefemomoney@gmail.com>
2012-11-09 16:06:01 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Patrick::Application.config.secret_token = 'e11bff449c9b03fcbf55636a0b659577b63a07f501539bf1a743fc7d0b37cd25ddc3ae4f6a330fbe03f20e730e0c8acd839b3ad365d044c380253f6243274a8e'
